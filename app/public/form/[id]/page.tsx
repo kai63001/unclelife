06152204e17245d7112c5bb436f0c5ce7fd00535
kai63001 <supanat0245@gmail.com>
2023-07-04 15:56:21 +0700

    update public form

--- app/public/form/[id]/page.tsx
+++ app/public/form/[id]/page.tsx
@@ -3,9 +3,9 @@ import FormMainBox from "@/app/custom/components/formMain";
 const PublicFormPage = ({ params }: { params: { id: string } }) => {
     console.log(params)
   return (
-    <div className="h-screen w-screen flex">
-      <div className="m-auto">
-        <div className="border-2 p-5 min-w-[500px] w-full rounded-sm">
+    <div className="h-screen w-screen lg:flex">
+      <div className="lg:m-auto">
+        <div className="border-2 p-5 lg:min-w-[500px] w-full rounded-sm">
           <FormMainBox id={params.id} />
         </div>
       </div>
