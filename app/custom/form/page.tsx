ca38a465e09bfb40d7433a7d81f6e931d1699a03
kai63001 <supanat0245@gmail.com>
2023-07-19 23:47:05 +0700

    add success page

--- app/custom/form/page.tsx
+++ app/custom/form/page.tsx
@@ -1,10 +1,24 @@
 import FormMainBox from "../components/formMain";
+import SuccessPageComponent from "../components/successPage";
 
 const CreateFormCustomPage = () => {
   return (
-    <div className="max-w-2xl w-full h-fit bg-background rounded-sm">
-      <div className="border-2 border-dashed p-5 w-full h-full rounded-sm">
-        <FormMainBox testMode={true} />
+    <div className="max-w-2xl w-full h-fit rounded-sm flex flex-col space-y-10">
+      <div>
+        <p className="mb-2">Page 1</p>
+        <div className="max-w-2xl w-full h-fit bg-background rounded-sm">
+          <div className="border-2 border-dashed p-5 w-full h-full rounded-sm">
+            <FormMainBox testMode={true} />
+          </div>
+        </div>
+      </div>
+      <div>
+        <p className="mb-2">Page Success</p>
+        <div className="max-w-2xl w-full h-fit bg-background rounded-sm">
+          <div className="border-2 border-dashed p-5 w-full h-full rounded-sm">
+            <SuccessPageComponent />
+          </div>
+        </div>
       </div>
     </div>
   );
