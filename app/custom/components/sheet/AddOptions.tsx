9a600b886244ac77806177b0a46c63d4e336c263
kai63001 <supanat0245@gmail.com>
2023-07-26 15:19:28 +0700

    update sheet select option

---
+++ app/custom/components/sheet/AddOptions.tsx
@@ -0,0 +1,93 @@
+"use client"
+import {Plus} from "lucide-react";
+import {Input} from "@/components/ui/input";
+import {useAppDispatch} from "@/app/redux/hook";
+import {setLayerWithId} from "@/app/redux/slice/formController.slice";
+
+const AddOptions = ({
+                        data
+                    }: {
+    data: any;
+}) => {
+    const dispatch = useAppDispatch();
+
+    const addOption = () => {
+        console.log('add option');
+        const newOption = {
+            name: `Option ${data?.options?.length + 1 || 1}`,
+            color: 'gray'
+        }
+        const listOptions: any = []
+        data?.options?.map((item: any) => {
+            listOptions.push(item)
+        })
+        listOptions.push(newOption)
+        const newData = {
+            ...data,
+            ['options']: listOptions
+        }
+        console.log('newData :', newData)
+        dispatch(setLayerWithId({
+            id: data?.id,
+            value: {
+                ...newData
+            }
+        }))
+    }
+
+    const updateNameWithId = (e: any, index: number) => {
+        const listOptions: any = []
+        data?.options?.map((item: any, i: number) => {
+            if (i === index) {
+                listOptions.push({
+                    ...item,
+                    ['name']: e
+                })
+            } else {
+                listOptions.push(item)
+            }
+        })
+        const newData = {
+            ...data,
+            ['options']: listOptions
+        }
+        console.log('newData :', newData)
+        dispatch(setLayerWithId({
+            id: data?.id,
+            value: {
+                ...newData
+            }
+        }))
+    }
+
+
+    return (
+        <div>
+            <span className={'text-lg font-medium'}>Select Options</span>
+            <div className={'mt-2 flex flex-col space-y-2'}>
+                {/* List Options */}
+                {data?.options?.map((item: any, index: number) => (
+                    <div key={index}>
+                        <Input
+                            key={index}
+                            onChange={(e) => {
+                                updateNameWithId(e.target.value, index)
+                            }}
+                            placeholder={'Option ' + (index + 1)}
+                            value={
+                                item?.name
+                            }/>
+                    </div>
+                ))}
+                <button
+                    onClick={addOption}
+                    className={'flex items-center text-gray-700 px-2 py-1 hover:bg-gray-200 duration-100 rounded-md'}>
+                    <Plus className={'h-4 w-4 mr-2'}/>
+                    Add Option
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default AddOptions;
