dbb9c1f7d89cf70b5cb1f26fcd6852f09e210f61
kai63001 <supanat0245@gmail.com>
2023-07-29 17:38:47 +0700

    update embed music

--- app/custom/components/button/ButtonEmbed.tsx
+++ app/custom/components/button/ButtonEmbed.tsx
@@ -1,6 +1,46 @@
+import {Button} from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import {Braces} from "lucide-react";
+
+
 const ButtonEmbed = () => {
     return (
-        <></>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button
+                    className={'bg-background px-5 py-3 rounded-md shadow-me font-medium flex items-center h-12 border-0 text-md'}
+                    variant="outline">
+                    <Braces className="mr-2 h-4 w-4"/>
+                    Embed Form
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[800px]">
+                <DialogHeader>
+                    <DialogTitle>Embed Form</DialogTitle>
+                    <DialogDescription>
+                        Pick one of the embed option below.
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="grid grid-cols-3 gap-4 py-4">
+                    <div className={'border rounded-md px-4 py-2 cursor-pointer hover:bg-secondary'}>
+                        Embed Form in Notion Page
+                    </div>
+                    <div className={'border rounded-md px-4 py-2 cursor-pointer hover:bg-secondary'}>
+                        Embed Form in Website
+                    </div>
+                    <div className={'border rounded-md px-4 py-2 cursor-pointer hover:bg-secondary'}>
+                        QR Code
+                    </div>
+                </div>
+            </DialogContent>
+        </Dialog>
     );
 };
 
--- app/custom/components/button/openForm.tsx
+++ app/custom/components/button/openForm.tsx
@@ -2,6 +2,7 @@
 import { useAppSelector } from "@/app/redux/hook";
 import Link from "next/link";
 import { FileSymlink } from "lucide-react";
+import {Button} from "@/components/ui/button";
 
 const OpenFormBtn = () => {
   const { infomation } = useAppSelector((state) => state.formReducer);
@@ -10,9 +11,11 @@ const OpenFormBtn = () => {
     <Link
       href={`${process.env.NEXT_PUBLIC_FRONT_END_URL}/public/form/${infomation.id}`}
       target="_blank"
-      className="bg-background px-5 py-3 rounded-md shadow-me font-medium flex items-center"
+      className="rounded-md shadow-me font-medium flex items-center"
     >
-      <FileSymlink className="mr-2 h-4 w-4" /> Open Form
+      <Button className={'bg-background px-5 py-3 rounded-md shadow-me font-medium flex items-center h-12 border-0 text-md'} variant="outline">
+        <FileSymlink className="mr-2 h-4 w-4" /> Open Form
+      </Button>
     </Link>
   ) : (
     <div className="bg-background text-gray-600 px-5 py-3 rounded-md shadow-me font-medium cursor-not-allowed flex items-center">
