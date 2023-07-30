43a1d78ec1423084bf6af4ae335cf3f405c6b785
kai63001 <supanat0245@gmail.com>
2023-07-30 14:20:29 +0700

    update

--- app/(dashboard)/components/ProfileBar.tsx
+++ app/(dashboard)/components/ProfileBar.tsx
@@ -5,6 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { LogOut, Settings } from "lucide-react";
 import Link from "next/link"
+import {ModeToggle} from "@/components/ModeToggle";
 
 const ProfileBar = async () => {
   const supabase = createServerComponentClient({ cookies });
@@ -32,7 +33,11 @@ const ProfileBar = async () => {
           </p>
         </div>
       </div>
+
       <div className="mt-5 flex">
+        <div className={'w-2/6 mr-2'}>
+          <ModeToggle/>
+        </div>
         <Link href={"/setting"} className="w-full mr-2">
           <Button className={'w-full '} variant={"outline"} >
             <Settings className="w-4 h-4" />
