95d0b7bed155af9018da9c397ae569b4555d4642
kai63001 <supanat0245@gmail.com>
2023-08-01 19:04:50 +0700

    feat: add embed from in website

--- app/custom/components/button/ButtonEmbed.tsx
+++ app/custom/components/button/ButtonEmbed.tsx
@@ -12,16 +12,19 @@ import {Braces, ArrowLeft} from "lucide-react";
 import {useState} from "react";
 import FormInNotion from "@/app/custom/components/button/embed/FormInNotion";
 import {useAppSelector} from "@/app/redux/hook";
+import FormInWebsite from "@/app/custom/components/button/embed/FormInWebsite";
 
 
 const ButtonEmbed = () => {
     const [selection, setSelection] = useState('')
-    const { infomation } = useAppSelector((state) => state.formReducer);
+    const {infomation} = useAppSelector((state) => state.formReducer);
 
     const selectionRender = () => {
         switch (selection) {
             case 'formInNotion':
                 return <FormInNotion/>
+            case 'formInWebsite':
+                return <FormInWebsite/>
             default:
                 return <>
                     <DialogHeader>
@@ -35,7 +38,8 @@ const ButtonEmbed = () => {
                              className={'border rounded-md px-4 py-2 cursor-pointer hover:bg-secondary'}>
                             Embed Form in Notion Page
                         </div>
-                        <div className={'border rounded-md px-4 py-2 cursor-pointer hover:bg-secondary'}>
+                        <div onClick={() => setSelection('formInWebsite')}
+                             className={'border rounded-md px-4 py-2 cursor-pointer hover:bg-secondary'}>
                             Embed Form in Website
                         </div>
                         <div className={'border rounded-md px-4 py-2 cursor-pointer hover:bg-secondary'}>
--- app/custom/components/button/embed/FormInWebsite.tsx
+++ app/custom/components/button/embed/FormInWebsite.tsx
@@ -4,12 +4,12 @@ import {Button} from "@/components/ui/button";
 import {useAppSelector} from "@/app/redux/hook";
 import {useToast} from "@/components/ui/use-toast";
 
-const FormInNotion = () => {
+const FormInWebsite = () => {
     const {toast} = useToast();
     const {infomation} = useAppSelector((state) => state.formReducer);
     const copyLink = (idData = infomation.id) => {
         navigator.clipboard.writeText(
-            `${process.env.NEXT_PUBLIC_FRONT_END_URL}/public/form/${idData}`
+            `<iframe src="${process.env.NEXT_PUBLIC_FRONT_END_URL}/public/form/${idData}" width="100%" height="500px" style="border:none;width:100%;"></iframe>`
         ).then(() => (
             toast({
                 title: "Copied",
@@ -21,29 +21,25 @@ const FormInNotion = () => {
     return (
         <>
             <DialogHeader>
-                <DialogTitle>Embed your form in a Notion Page</DialogTitle>
+                <DialogTitle>Embed your form in a website</DialogTitle>
                 <DialogDescription>
-                    Embedding your form into your Notion page is a simple process. Begin by copying the URL of your form
-                    provided below.
+                    Integrate your form into your website by duplicating the HTML code provided below.
                 </DialogDescription>
             </DialogHeader>
             <div>
                 <div className={'border rounded-md px-5 py-2 flex items-center justify-between'}>
                     <span className={'text-sm'}>
-                        {`${process.env.NEXT_PUBLIC_FRONT_END_URL}/public/form/${infomation.id}`}
+                        {`
+                            <iframe src="${process.env.NEXT_PUBLIC_FRONT_END_URL}/public/form/${infomation.id}" width="100%" height="500px" style="border:none;width:100%;"></iframe>
+                        `}
                     </span>
                     <Button onClick={() => copyLink()}>
                         COPY
                     </Button>
                 </div>
-                <p className={'my-5'}>
-                    Next, navigate to your preferred page in Notion, initiate an {`'`}embed component{`'`} by
-                    typing <span className={'bg-red-600 px-2 py-0.5 rounded-md'}>/embed</span>, and effortlessly paste
-                    the link you{`'`}ve just copied!
-                </p>
             </div>
         </>
     );
 };
 
-export default FormInNotion;
+export default FormInWebsite
