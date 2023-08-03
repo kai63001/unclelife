8ed468ee94446b7347b7d18ccabd217d84bac62f
kai63001 <supanat0245@gmail.com>
2023-08-03 13:53:40 +0700

    update

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -9,7 +9,7 @@ const PricingPage = () => {
             <section className="max-w-4xl mx-auto w-full flex-col justify-center pt-16 items-center relative">
                 <h1 className={'font-bold text-5xl text-center'}>Enhance your experience with our <span className={'bg-gradient-to-r from-pink-500 to-rose-500 bg-clip-text text-transparent'}>Professional</span> plans</h1>
                 <p className={'text-muted-foreground text-center my-2'}>
-                    Upgrade your Notion form creation with our Pro plans. Enjoy advanced features and superior support. Choose the plan that fits your needs best.
+                    Upgrade your Notion form and widget creation with our Pro plans. Enjoy advanced features and superior support. Choose the plan that fits your needs best.
                 </p>
             </section>
         </main>
