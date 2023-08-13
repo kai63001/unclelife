b83a1ec6430f28a84339cfa2c1e13e712ce3a4ad
kai63001 <supanat0245@gmail.com>
2023-08-13 19:09:52 +0700

    add pro badget

--- app/custom/components/toolsbar/CustomizationToolbar.tsx
+++ app/custom/components/toolsbar/CustomizationToolbar.tsx
@@ -1,10 +1,16 @@
 import {AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion";
 import {Button} from "@/components/ui/button";
+import {Upload} from "lucide-react";
+import ProBadge from "@/app/custom/components/toolsbar/ProBadge";
 
 const CustomizationToolbar = ({onChangeHook, form}: any) => {
     return (
         <AccordionItem value="customization">
-            <AccordionTrigger>Customization</AccordionTrigger>
+            <AccordionTrigger className={'hover:no-underline'}>
+                <div>
+                    Customization <ProBadge/>
+                </div>
+            </AccordionTrigger>
             <AccordionContent>
                 <div className="flex flex-col space-y-2">
                     <p className="text-xs font-bold">
@@ -12,8 +18,8 @@ const CustomizationToolbar = ({onChangeHook, form}: any) => {
                     </p>
                     <Button>
                         Upload Image
+                        <Upload className={'w-4 h-4 ml-2'}/>
                     </Button>
-
                 </div>
             </AccordionContent>
         </AccordionItem>
---
+++ app/custom/components/toolsbar/ProBadge.tsx
@@ -0,0 +1,7 @@
+const ProBadge = () => {
+    return (
+        <span className={'rounded-full px-3 py-0.5 text-xs bg-red-700'}>PRO</span>
+    )
+}
+
+export default ProBadge
