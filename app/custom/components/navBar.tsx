1f7a78b5fa063850b6f9a7560264a7c97c251541
kai63001 <supanat0245@gmail.com>
2023-08-27 16:42:01 +0700

    update and add og image

--- app/custom/components/navBar.tsx
+++ app/custom/components/navBar.tsx
@@ -11,7 +11,7 @@ const CustomFormNavbar = async () => {
     return (
         <div className="mt-5 pl-5 pr-5 flex w-full justify-between items-center fixed z-50">
             <div className="flex space-x-3 items-center">
-                <Link href={"/home"} className="bg-background px-5 py-2 rounded-md shadow-me">
+                <Link href={"/form/create"} className="bg-background px-5 py-2 rounded-md shadow-me">
                     <h1 className="font-bold text-2xl">Uncle Life</h1>
                 </Link>
                 <OpenFormBtn/>
