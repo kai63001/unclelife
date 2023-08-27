e75e2fd0c59941a72a44b9bb2955395486729622
kai63001 <supanat0245@gmail.com>
2023-08-27 19:53:47 +0700

    update blog

---
+++ _posts/evolution-of-form-creation.md
@@ -0,0 +1,34 @@
+---
+title: "The Evolution of Form Creation: Embracing UncleLife.co"
+description: "Explore the transformative journey of form creation, from traditional methods to the innovative solutions offered by UncleLife.co. Dive deep into the integration of Notion forms and the art of creating free forms."
+date: 2023-07-27
+cover: /image/blog/evolution-of-form-creation.webp
+---
+
+**The Renaissance of Form Creation with [UncleLife.co](https://unclelife.co)**
+
+In the vast expanse of the digital realm, the art of form creation has undergone a metamorphosis. From rudimentary checkboxes and text fields to dynamic, interactive platforms, the evolution is palpable. Central to this transformation is [UncleLife.co](https://unclelife.co), a beacon for those seeking to create free forms with finesse.
+
+[picture of a digital form evolving over time]
+
+**The Genesis of [UncleLife.co](https://unclelife.co)**
+
+The inception of [UncleLife.co](https://unclelife.co) was not merely to add another form creator to the digital quiver. It was a response to a clarion call for simplicity meshed with functionality. The platform's ethos is rooted in providing a seamless experience for both novices and veterans in the realm of form creation.
+
+**Notion Forms: A Symphony of Integration**
+
+The integration of [Notion forms](https://notion.so/forms) is not just a feature; it's a revolution. For those entrenched in the Notion ecosystem, this integration is akin to a symphonic blend of two powerful instruments. The result? A harmonious data collection process that resonates with efficiency.
+
+[picture of a Notion form seamlessly integrated with other tools]
+
+**Human Touch in a Digital World**
+
+Despite the technological advancements, [UncleLife.co](https://unclelife.co)'s commitment to preserving the human touch is unwavering. It's not just about creating forms; it's about crafting experiences. Every drag, every drop, every click is a testament to the platform's dedication to its users.
+
+[picture of a human playing a game, symbolizing engagement and interaction]
+
+**In Conclusion: The Future is Here**
+
+As we stand at the crossroads of digital innovation, [UncleLife.co](https://unclelife.co) emerges as a vanguard of form creation. It's not just a tool; it's a movement. A movement towards efficiency, integration, and above all, a human-centric approach.
+
+Join the revolution. Embrace the future with [UncleLife.co](https://unclelife.co).
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,11 +2,14 @@ import Link from 'next/link'
 import {getAllPosts} from '@/lib/apiMarkdown'
 import IndexNavbar from "@/app/index/components/Navbar";
 import {Metadata} from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
     title: 'Uncle Life Blog - Insights on Notion Forms & Widgets',
     description: 'Dive into the Uncle Life Blog for expert insights, tips, and articles on maximizing Notion with our specialized forms and widgets. Stay updated and enhance your Notion journey with us.',
 }
+
+export const revalidate = 3600; // 1 hour
 
 export default async function Page() {
     const posts = await getAllPosts()
@@ -16,19 +19,25 @@ export default async function Page() {
             <header>
                 <IndexNavbar/>
             </header>
-            <section className="max-w-5xl mx-auto w-full flex-col justify-between pt-8 items-center relative">
+            <section className="max-w-6xl mx-auto w-full flex-col justify-between pt-8 items-center relative md:px-5 px-5">
                 <h1 className="text-center text-5xl font-extrabold mt-8">
                     Your Hub for Notion <span
                     className={'bg-gradient-to-r from-pink-500 to-rose-500 bg-clip-text text-transparent'}>Forms</span> and <span
                     className={'bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent'}>Widgets</span> Insights
                 </h1>
-                <ul>
+                <ul className={'grid grid-cols-1 md:grid-cols-3 gap-4 mt-10'}>
                     {posts.map(post => {
-                        const {id, date, title} = post
+                        const {id, date, title, cover} = post
                         return (
                             <li key={id}>
                                 <Link href={`/blog/${id}`}>
-                                    {date} - {title}
+                                    <div className={'relative h-52 overflow-hidden rounded-md'}>
+                                        <Image src={cover} alt={`Cover Image for ${title}`} className={'object-cover'}
+                                               fill/>
+                                    </div>
+                                    <p className={'font-medium mt-1'}>
+                                        {title}
+                                    </p>
                                 </Link>
                             </li>
                         )
