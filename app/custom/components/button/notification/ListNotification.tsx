4e993e56744c9d07d2b7b1bc270a2a21df73836d
kai63001 <supanat0245@gmail.com>
2023-10-18 11:31:39 +0700

    fix

--- app/custom/components/button/notification/ListNotification.tsx
+++ app/custom/components/button/notification/ListNotification.tsx
@@ -1,11 +1,36 @@
-"use client"
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Send, Mail } from "lucide-react";
+import { useState } from "react";
 
 const ListNotification = () => {
-    return (
-        <div>
-            asd
-        </div>
-    )
-}
+  const [selectNotification, setSelectNotification]: any = useState("none");
 
-export default ListNotification
+  if (selectNotification == "RespondentEmail") {
+    return <div>asd</div>;
+  }
+
+  return (
+    <div className="flex flex-col space-y-3">
+      <Button
+        onClick={() => setSelectNotification("RespondentEmail")}
+        variant={"outline"}
+        className="py-5"
+      >
+        <Send className="h-4 w-4 mr-2" />
+        Respondent Email Notifications
+      </Button>
+      <Button
+        variant={"outline"}
+        className="py-5"
+        onClick={() => setSelectNotification("SelfEmail")}
+      >
+        <Mail className="h-4 w-4 mr-2" />
+        Self Email Notifications
+      </Button>
+    </div>
+  );
+};
+
+export default ListNotification;
