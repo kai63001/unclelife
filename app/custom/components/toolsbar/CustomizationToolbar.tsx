41a1ef8443c17b48cb1be98b12641e0a404a1669
kai63001 <supanat0245@gmail.com>
2023-10-23 19:53:21 +0700

    update dark mode selectiong

--- app/custom/components/toolsbar/CustomizationToolbar.tsx
+++ app/custom/components/toolsbar/CustomizationToolbar.tsx
@@ -3,6 +3,7 @@ import ProBadge from "@/app/custom/components/toolsbar/ProBadge";
 import UploadCoverPicture from "@/app/custom/components/toolsbar/Customize/UploadCoverPicture";
 import HideBranding from "@/app/custom/components/toolsbar/Customize/HideBranding";
 import UploadLogoPicture from "./Customize/UploadLogoPicture";
+import { DarkmodeSelectionForm } from "./Customize/DarkmodeSelection";
 
 const CustomizationToolbar = ({onChangeHook, form}: any) => {
     return (
@@ -17,6 +18,7 @@ const CustomizationToolbar = ({onChangeHook, form}: any) => {
                     <UploadCoverPicture onChangeHook={onChangeHook} form={form}/>
                     <UploadLogoPicture onChangeHook={onChangeHook} form={form}/>
                     <HideBranding onChangeHook={onChangeHook} form={form}/>
+                    <DarkmodeSelectionForm onChangeHook={onChangeHook} form={form}/>
                 </div>
             </AccordionContent>
         </AccordionItem>
---
+++ app/custom/components/toolsbar/FreeBadge.tsx
@@ -0,0 +1,7 @@
+const FreeBadge = () => {
+  return (
+    <span className={'rounded-md ml-2 mb-1 px-2 py-0.5 text-xs bg-blue-100 text-blue-600 font-bold'}>FREE</span>
+  );
+};
+
+export default FreeBadge;
